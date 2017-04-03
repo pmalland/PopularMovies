e2ba35d72fd50f7c38a07c7a86e4fe8fed6eeb77
pmalland <paul.malland@gmail.com>
2017-04-03 20:24:55 +0200

    add thread to internet call

--- app/src/main/java/com/exemple/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/exemple/android/popularmovies/MainActivity.java
@@ -1,13 +1,106 @@
 package com.exemple.android.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.exemple.android.popularmovies.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+
+//    A text view for testing purposes
+    private TextView mSearchResultDisplay;
+    private TextView mErrorMessageTextView;
+    private ProgressBar mLoadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        mSearchResultDisplay = (TextView) findViewById(R.id.test_text_text_view);
+        mErrorMessageTextView = (TextView) findViewById(R.id.error_message_tv);
+        mLoadingIndicator = (ProgressBar) findViewById(R.id.loading_indicator_pb);
+    }
+
+
+    private void makeTheMovieDBSearchQuery(int queryCOde){
+        URL theMovieDBSearchURL = NetworkUtils.buildUrl(queryCOde);
+        new MovieDBQueryTask().execute(theMovieDBSearchURL);
+
+
+    }
+
+    private void showDataView(){
+        mErrorMessageTextView.setVisibility(View.INVISIBLE);
+        mSearchResultDisplay.setVisibility(View.VISIBLE);
+    }
+
+    private void shorErrorMessage(){
+        mSearchResultDisplay.setVisibility(View.INVISIBLE);
+        mErrorMessageTextView.setVisibility(View.VISIBLE);
+    }
+
+    public class MovieDBQueryTask extends AsyncTask<URL,Void,String>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(URL... params) {
+            URL searchURL = params[0];
+            String movieDBSearchResults = null;
+            try {
+                movieDBSearchResults = NetworkUtils.getResponseFromHttpUrl(searchURL);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
+            return movieDBSearchResults;
+        }
+
+        @Override
+        protected void onPostExecute(String movieDBSearchResults) {
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+            if(movieDBSearchResults != null && !movieDBSearchResults.equals("")){
+                mSearchResultDisplay.setText(movieDBSearchResults);
+            } else {
+                shorErrorMessage();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.movie,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemThatWhatSelected = item.getItemId();
+        if(menuItemThatWhatSelected == R.id.action_sort){
+            makeTheMovieDBSearchQuery(100);
+            Context context = MainActivity.this;
+            String message = getString(R.string.toast_sort);
+            Toast.makeText(context,message,Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/exemple/android/popularmovies/utilities/NetworkUtils.java
@@ -0,0 +1,77 @@
+package com.exemple.android.popularmovies.utilities;
+
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetworkUtils {
+
+    private final static String THEMOVIEDB_BASE_URL = "http://api.themoviedb.org/3";
+
+    private final static String PARAM_POPULAR ="/movie/popular";
+    private final static String PARAM_RATE = "/movie/top_rated";
+    private final static String POPULAR_QUERY_BASE_URL = THEMOVIEDB_BASE_URL + PARAM_POPULAR ;
+    private final static String RATE_QUERY_BASE_URL = THEMOVIEDB_BASE_URL + PARAM_RATE;
+    private final static String PARAM_API_KEY = "api_key";
+
+    private final static int POPULAR_KEY = 100;
+    private final static int RATE_KEY= 110;
+
+    private final static String API_KEY_VALUE = "";
+
+
+    public static URL buildUrl(int REQUEST_KEY){
+        String QUERY_BASE_URL = null;
+        if(REQUEST_KEY == POPULAR_KEY){
+            QUERY_BASE_URL = POPULAR_QUERY_BASE_URL;
+        } else if (REQUEST_KEY == RATE_KEY){
+            QUERY_BASE_URL = RATE_QUERY_BASE_URL;
+        }
+        Uri ourBuiltUri = Uri.parse(QUERY_BASE_URL).buildUpon()
+                .appendQueryParameter(PARAM_API_KEY,API_KEY_VALUE)
+                .build();
+
+        URL url = null;
+
+        try{
+            url= new URL(ourBuiltUri.toString());
+
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try{
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+           scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            String response = null;
+            if(hasInput){
+                response = scanner.next();
+            }
+            scanner.close();
+            return response;
+        }finally {
+            urlConnection.disconnect();
+       }
+
+   }
+}
+
+
+
+
